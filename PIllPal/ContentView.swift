97f02ffc6e36b45bbff86e73b2f77330ddd2d47e
mathissjsp <mathis.goffin@jopie.be>
2025-04-29 13:39:09 +0200

    Initial Commit

---
+++ PIllPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PIllPal
+//
+//  Created by mathis goffin on 29/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PIllPal/PIllPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIllPalApp.swift
+//  PIllPal
+//
+//  Created by mathis goffin on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PIllPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
