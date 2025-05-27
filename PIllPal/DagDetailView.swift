3ef9725a7555965874effd080d14bf66ba38d9ee
mathissjsp <mathis.goffin@jopie.be>
2025-05-27 11:41:01 +0200

    first commit

---
+++ PIllPal/DagDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DagDetailView.swift
+//  PIllPal
+//
+//  Created by mathis goffin on 15/05/2025.
+//
+
+import SwiftUI
+
+struct DagDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DagDetailView()
+}
---
+++ PIllPal/KalenderView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PIllPal
+//
+//  Created by mathis goffin on 29/04/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
